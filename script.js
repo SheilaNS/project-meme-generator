e4838e79d779de5b36f8e6238591555274897f91
SheilaNS <shei.nsantos@gmail.com>
2021-12-21 23:07:29 -0300

    Requisitos 2 e 3 ok

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const inputTexto = document.getElementById('text-input'); // input
 // const memeImage = document.getElementById('meme-image-container'); // meme-div
 const memeText = document.getElementById('meme-text'); // meme-text-div
-// const botaoAdd = document.getElementById('text-add'); // botão add
+// const memeImage = document.getElementById('meme-image'); // img meme-image
+const inputImage = document.getElementById('meme-insert'); // input file
+// const sendButton = document.getElementById('meme-send'); // botão enviar
 
 function addText() {
   const texto = inputTexto.value;
@@ -9,3 +11,16 @@ function addText() {
 }
 
 inputTexto.addEventListener('keyup', addText);
+
+/* function clearImage(src) {
+  return URL.revokeObjectURL(src);
+}
+ */
+const addImage = function (event) {
+  const memeImage = document.getElementById('meme-image');
+  memeImage.src = (URL.createObjectURL(event.target.files[0]));
+  memeImage.onload = function () {
+    (URL.revokeObjectURL(memeImage.src));
+  };
+  console.log(event.target.files[0]);
+};
