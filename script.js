aaf71d8e72b40458f5e9828b893d219732ca680e
SheilaNS <shei.nsantos@gmail.com>
2021-12-22 14:40:34 -0300

    Arrumando os requisitos e colocando referências/ajudas

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputTexto = document.getElementById('text-input'); // input
 const memeContainer = document.getElementById('meme-image-container'); // meme-div
+const memeImage = document.getElementById('meme-image');
 const memeText = document.getElementById('meme-text'); // meme-text-div
 const fireButton = document.getElementById('fire'); // botão fogo
 const waterButton = document.getElementById('water'); // botão água
@@ -9,6 +10,7 @@ const meme2 = document.getElementById('meme-2');
 const meme3 = document.getElementById('meme-3');
 const meme4 = document.getElementById('meme-4');
 
+// Requisito 01 - Recebido ajuda do Thiago Zardo
 function addText() {
   const texto = inputTexto.value;
   memeText.innerText = texto;
@@ -16,14 +18,16 @@ function addText() {
 
 inputTexto.addEventListener('keyup', addText);
 
+// Requisito 02 - Feito com a ajuda do Thiago Zardo
+// Referência das dicas do projeto: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
 const addImage = function (event) {
-  const memeImage = document.getElementById('meme-image');
   memeImage.src = (URL.createObjectURL(event.target.files[0]));
   memeImage.onload = function () {
     (URL.revokeObjectURL(memeImage.src));
   };
 };
 
+// Requisito 06
 function addFireBorder() {
   memeContainer.style.border = '3px dashed red';
 }
@@ -40,29 +44,26 @@ fireButton.addEventListener('click', addFireBorder);
 waterButton.addEventListener('click', addWaterBorder);
 earthButton.addEventListener('click', addEarthBorder);
 
+// Requisito 07
 function addMeme1() {
-  const memeImage = document.getElementById('meme-image');
   memeImage.src = '/imgs/meme1.png';
   memeImage.onload = function () {
     (URL.revokeObjectURL(memeImage.src));
   };
 }
 function addMeme2() {
-  const memeImage = document.getElementById('meme-image');
   memeImage.src = '/imgs/meme2.png';
   memeImage.onload = function () {
     (URL.revokeObjectURL(memeImage.src));
   };
 }
 function addMeme3() {
-  const memeImage = document.getElementById('meme-image');
   memeImage.src = '/imgs/meme3.png';
   memeImage.onload = function () {
     (URL.revokeObjectURL(memeImage.src));
   };
 }
 function addMeme4() {
-  const memeImage = document.getElementById('meme-image');
   memeImage.src = '/imgs/meme4.png';
   memeImage.onload = function () {
     (URL.revokeObjectURL(memeImage.src));
