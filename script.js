b2b54ba9e3a83f7fa4877f11e5a63f263a24adfb
SheilaNS <shei.nsantos@gmail.com>
2022-01-19 15:24:52 -0300

    Agradando o Lint

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputTexto = document.getElementById('text-input'); // input
 const memeContainer = document.getElementById('meme-image-container'); // meme-div
 const memeImage = document.getElementById('meme-image');
+const memeInsert = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text'); // meme-text-div
 const fireButton = document.getElementById('fire'); // botão fogo
 const waterButton = document.getElementById('water'); // botão água
@@ -20,9 +21,9 @@ inputTexto.addEventListener('keyup', addText);
 
 // Requisito 02 - Feito com a ajuda do Thiago Zardo
 // Referência das dicas do projeto: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
-const addImage = function (event) {
+memeInsert.onchange = (event) => {
   memeImage.src = (URL.createObjectURL(event.target.files[0]));
-  memeImage.onload = function () {
+  memeImage.onload = () => {
     (URL.revokeObjectURL(memeImage.src));
   };
 };
@@ -47,25 +48,25 @@ earthButton.addEventListener('click', addEarthBorder);
 // Requisito 07
 function addMeme1() {
   memeImage.src = '/imgs/meme1.png';
-  memeImage.onload = function () {
+  memeImage.onload = () => {
     (URL.revokeObjectURL(memeImage.src));
   };
 }
 function addMeme2() {
   memeImage.src = '/imgs/meme2.png';
-  memeImage.onload = function () {
+  memeImage.onload = () => {
     (URL.revokeObjectURL(memeImage.src));
   };
 }
 function addMeme3() {
   memeImage.src = '/imgs/meme3.png';
-  memeImage.onload = function () {
+  memeImage.onload = () => {
     (URL.revokeObjectURL(memeImage.src));
   };
 }
 function addMeme4() {
   memeImage.src = '/imgs/meme4.png';
-  memeImage.onload = function () {
+  memeImage.onload = () => {
     (URL.revokeObjectURL(memeImage.src));
   };
 }
